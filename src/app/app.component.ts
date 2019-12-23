4ce8624ab72788919ea58f35ca79b51251d257ef
FerozAhmadLonpo <ajjadhussain786@gmail.com>
2019-12-23 01:25:44 -0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { User } from "./user";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'tdf';
+  topics = ["Angular", "React", "Vue"];
+  topicHasError = true;
+
+  userModel = new User("", "rob@test.com", 54545645654, "default", "morning", true);
+
+  validateTopic(value){
+    if(value == 'default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
+  }
 }
